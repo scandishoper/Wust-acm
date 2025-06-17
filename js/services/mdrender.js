1871178aef9c2c6461d48c9fd3c487fa0bb2dbb7
scandi <3567140151@qq.com>
2025-06-18 02:10:53 +0800

    Site updated: 2025-06-18 02:10:53

---
+++ js/services/mdrender.js
@@ -0,0 +1,13 @@
+utils.jq(() => {
+  const els = document.getElementsByClassName('ds-mdrender');
+  for (var i = 0; i < els.length; i++) {
+    const el = els[i];
+    const src = `${el.getAttribute('src')}?t=${new Date().getTime()}`;
+    console.log('src', src);
+    
+    utils.request(el, src, async resp => {
+      const data = await resp.text();
+      el.innerHTML = marked.parse(data);
+    });
+  }
+});
